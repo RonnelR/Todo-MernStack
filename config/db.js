0d20618f385961de2dcc3a90d39e5dc505419e4a
RonnelR <91006865+RonnelR@users.noreply.github.com>
2025-10-17 11:16:39 +0530

    fix bugs

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const databaseConnection = async() =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`database connected successfull ${conn.connection.host} `.bgBlue.white)
+    } catch (error) {
+        console.log('error in database connection'.bgRed.white)
+        }
+}
